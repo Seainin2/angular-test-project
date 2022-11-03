5b5a49ff001a1ff64f5a528c6b4486a18683151a
SeaininLaptop <seainin2@gmail.com>
2022-11-03 14:54:34 +0000

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 import { EmpInfoComponent } from './emp-info/emp-info.component';
+import { FormcomponentComponent } from './formcomponent/formcomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextcomponentComponent,
     ImagecomponentComponent,
-    EmpInfoComponent
+    EmpInfoComponent,
+    FormcomponentComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formcomponent/formcomponent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formcomponent',
+  templateUrl: './formcomponent.component.html',
+  styleUrls: ['./formcomponent.component.css']
+})
+export class FormcomponentComponent{
+
+  submit(login: any){
+
+    console.log("Form Submitted!!",login)
+
+  }
+
+}
